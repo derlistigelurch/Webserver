20755194c7e9f0c6bf2ef20b5bbc532538ad8eb5
derlistigelurch <dalistige@mail.com>
2019-09-30 23:46:27 +0200

    added some lines of code

--- Webserver/Program.cs
+++ Webserver/Program.cs
@@ -7,6 +7,25 @@ namespace Webserver
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			String MyString = "/test.jpg?x=y?y=foo";
+			
+			// split string
+			String[] Elements = MyString.Split('?');
+			int ParamCount = Elements.Length - 1;
+			
+			foreach (var strings in Elements)
+			{
+				//first string = path
+				//second ... parameter
+				Console.WriteLine(strings);
+			}
+			
+			//return parameter count
+			Console.WriteLine(ParamCount);
+			
+			//return raw url
+			Console.WriteLine(MyString);
+			
 		}
 	}
 }
--- Webserver/Url.cs
+++ Webserver/Url.cs
@@ -4,19 +4,20 @@ using BIF.SWE1.Interfaces;
 
 namespace Webserver
 {
-	public class Url : IUrl
-	{
-		public Url(string url)
-		{
-			this.Path = url;
-		}
-		public string RawUrl { get; }
-		public string Path { get; }
-		public IDictionary<string, string> Parameter { get; }
-		public int ParameterCount { get; }
-		public string[] Segments { get; }
-		public string FileName { get; }
-		public string Extension { get; }
-		public string Fragment { get; }
-	}
+    public class Url : IUrl
+    {
+        public Url(string url)
+        {
+            this.Path = url;
+        }
+
+        public string RawUrl { get; }
+        public string Path { get; }
+        public IDictionary<string, string> Parameter { get; }
+        public int ParameterCount { get; }
+        public string[] Segments { get; }
+        public string FileName { get; }
+        public string Extension { get; }
+        public string Fragment { get; }
+    }
 }
